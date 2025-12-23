abb54a00530270d6ebdf60db3b0a108390901de9
Morium-Undyning <106692310+Morium-Undyning@users.noreply.github.com>
2025-12-23 14:55:25 +0700

    1

--- Assets/Scritps/Res.cs
+++ Assets/Scritps/Res.cs
@@ -4,4 +4,27 @@ public class Res : MonoBehaviour,IRes
 {
     public enum Resourses {Wood, Stone, OilCristall }
     public Resourses ress;
+    public Renderer rd;
+    void Start()
+    {
+        rd = GetComponent<Renderer>();
+        int i = Random.Range(0, 2);
+        if(i == 0)
+        {
+            ress = Resourses.Wood;
+            rd.material.color = new Color(0.65f, 0.46f, 0.16f);
+        }
+        else if(i == 1)
+        {
+            ress = Resourses.Stone;
+            rd.material.color = new Color(0.5f, 0.5f, 0.5f);
+        }
+        /*
+        else if(i == 2)
+        {
+            ress = Resourses.OilCristall;
+            rd.material.color = new Color(0.1f, 0.1f, 0.2f);
+        }
+        */
+    }
 }
--- Assets/Scritps/ResGeneration.cs
+++ Assets/Scritps/ResGeneration.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class ResGeneration : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject resPrefabs;
+    [SerializeField] private float Tiner = 0;
+    [SerializeField] private float UseTime = 3f;
+    [SerializeField] private int RadiosGenerate = 5;
+
     void Start()
     {
         
     }
+    void Generate()
+    {
+        if(Tiner <= 0)
+        {   
+            GenerateRes(resPrefabs,RadiosGenerate);
+            Tiner = UseTime;
+        }
+        else
+        {
+            Tiner -= Time.deltaTime;
+        }
+    }
 
-    // Update is called once per frame
+    void GenerateRes(GameObject rp, int radios)
+    {
+        //float x = Random.Range(this.transform.position.x - radios, +radios);
+        Vector2 r = Random.insideUnitCircle * radios;
+        Vector3 Gen = new Vector3(transform.position.x+r.x,transform.position.y,transform.position.z+r.y);
+        GameObject res = Instantiate(rp,Gen,Quaternion.identity);
+
+    }
+
+    
     void Update()
     {
-        
+        Generate();
     }
 }
